3a0c4d8df3e09b519117b7af9b0645142dae1dab
Ömer <76915702+omergzlaydn@users.noreply.github.com>
2024-07-01 00:16:24 +0300

    -

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -10,7 +10,7 @@ const MovieListContainer: React.FC<MovieListContainerProps> = ({ title, endpoint
   return (
     <div className="bg-gradient-to-r from-red-600 via-burgundy-700 to-black rounded-lg p-4">
       <h2 className="text-3xl font-bold text-white mb-4">{title}</h2>
-      <div className="overflow-x-scroll scrollbar-hide">
+      <div>
         <MovieList endpoint={endpoint} title={""} />
       </div>
     </div>
@@ -20,9 +20,9 @@ const MovieListContainer: React.FC<MovieListContainerProps> = ({ title, endpoint
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col gap-8 bg-gray-900 text-white p-6">
-      <MovieListContainer title="Popular" endpoint="/movie/popular" />
-      <MovieListContainer title="Top Rated" endpoint="/movie/top_rated" />
-      <MovieListContainer title="Trending" endpoint="/trending/movie/day" />
+      <MovieListContainer title="Popüler" endpoint="/movie/popular" />
+      <MovieListContainer title="En İyi Oy Alanlar" endpoint="/movie/top_rated" />
+      <MovieListContainer title="Trendler" endpoint="/trending/movie/day" />
     </div>
   );
 };
