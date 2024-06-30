fb3a6497b8334cce33f15168cc8f17855df674ab
Ömer <76915702+omergzlaydn@users.noreply.github.com>
2024-06-30 10:46:35 +0300

    -

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <nav className="flex space-x-8">
           <Link className="flex items-center gap-2 text-lg font-medium hover:text-red-500 transition-colors" to="/favorites">
             <MdBookmarkBorder size={24} color="#F6F1D3" />
-            FAVORİTES
+            FAVORITES
           </Link>
         </nav>
       </div>
